1b670a179add65ccf697c8b7898d3f32b1e99a58
JCB <jeanclaude.bartier@gmail.com>
2015-03-22 16:56:50 +0100

    maj

---
+++ mes_constantes.R
@@ -0,0 +1,35 @@
+# CONSTANTES
+#===============
+
+pop.als.2010.totale<-1115226 + 765634
+pop_als_2010_municipale<-1095905 + 749782
+pop.67.2010.municipale<-1095905
+pop.68.2010.municipale<-749782
+
+pop0<-21655
+pop1_75<-1677958
+pop75<-146074
+pop1_15<-309641
+pop15_75<-1368317
+pop75_85<-108426
+pop85<-37647
+
+hop.short <- c("Wis","Hag","Sav","Hus","Odi","Sel","Col","Geb","Mul","3Fr","Alk","Dia")
+
+hop.long <- c("CH Wissembourg","CH Haguenau","CH Saverne","HUS","CL Ste Odile","CH Séletat","CH Colmar","CH Guebwiller",
+              "CH Mulhouse", "CL 3 Frontières","CH Alkirch","Diaconat-Fonderie")
+
+rpu.names <- c("Entrée","Sexe","Age","Commune","ZIP","Provenance","PEC Transport",
+               "Mode Transport","Mode entrée","CCMU","Motif","DP","Sortie", "Mode sortie","Orientation","Destination")
+
+week.short <- c("Dim","Lun","Mar","Mer","Jeu","Ven","Sam")
+
+week.long <- c("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi")
+
+french.short.week <- c("Lun","Mar","Mer","Jeu","Ven","Sam","Dim")
+
+french.long.week <- c("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche")
+
+mois.short <- c("Jan","Fev","Mar","Avr","Mai","Jun","Jui","Aou","Sep","Oct","Nov","Dec")
+
+mois.long <- c("Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Decembre")
